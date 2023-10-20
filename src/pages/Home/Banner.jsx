e6acb19a4b42b54fcf8ce5c611d0950139a17c6b
Arnab Gupta <arnabgupta1111@gmail.com>
2023-10-20 13:12:04 +0600

    product details page and banner section design complete

--- src/pages/Home/Banner.jsx
+++ src/pages/Home/Banner.jsx
@@ -1,15 +1,18 @@
 const Banner = () => {
   return (
     <div>
-      <div className="hero min-h-[500px] md:min-h-[620px]" style={{ backgroundImage: "url(https://i.ibb.co/JQ0mSqX/banner1.jpg)" }}>
-        <div className="hero-overlay bg-opacity-60"></div>
-        <div className="hero-content text-center text-white">
-          <div className="max-w-5xl flex md:mt-8">
-            <div>
-              <h1 className="mb-5 text-3xl font-bold md:text-5xl drop-shadow-lg md:font-extrabold text-[#FF6A25]" data-aos="zoom-in">
+      <div
+        className="w-full bg-cover bg-center min-h-[500px] md:min-h-[620px]"
+        style={{ backgroundImage: "url(https://i.ibb.co/w6bSGw2/bg-header.jpg)" }}
+      >
+        <div className="hero-overlay bg-opacity-20"></div>
+        <div className="text-center">
+          <div className=" flex justify-center  md:">
+            <div className="max-w-5xl mt-14">
+              <h1 className="mb-5 text-3xl font-bold md:text-5xl drop-shadow-xl md:font-extrabold text-[#FF6A25]" data-aos="zoom-in">
                 A new Level of Comfort Tested for <br /> <span className="font-bold">your Safety</span>
               </h1>
-              <p className="mb-5" data-aos="zoom-in">
+              <p className="mb-5 text-[#2d373c]" data-aos="zoom-in">
                 Start Your Automotive Journey with a Warm Welcome.
               </p>
             </div>
--- src/pages/ProductDetails/ProductDetails.jsx
+++ src/pages/ProductDetails/ProductDetails.jsx
@@ -2,10 +2,24 @@ import { useLoaderData } from "react-router-dom";
 
 const ProductDetails = () => {
   const product = useLoaderData();
+  const { image, name, type, short_description, price } = product;
   console.log(product);
   return (
-    <div>
-      <h2>Details</h2>
+    <div className="bg-[#f7f1e6] min-h-screen ">
+      <div className="grid grid-cols-5 gap-5 pt-10 items-center">
+        <div className="col-span-3">
+          <img className="w-full h-[600px]" src={image} alt="" />
+        </div>
+        <div className="col-span-2 px-5 ">
+          <h2 className="text-[#2d373c] font-semibold text-4xl">{name}</h2>
+          <h4 className="text-[#2d373c] font-medium text-2xl mt-1">{type}</h4>
+          <p className="text-gray-600 font-medium text-base text-justify mt-3">{short_description}</p>
+          <h2 className="text-3xl font-bold text-[#FF6A25] mt-3">{price}</h2>
+          <button className="w-full hover:border-2 text-white hover:border-[#FF6A25] hover:bg-white hover:text-[#FF6A25] font-medium bg-[#FF6A25] py-3 text-[xl] hover:transition hover:duration-500 mt-8">
+            Add to Cart
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
